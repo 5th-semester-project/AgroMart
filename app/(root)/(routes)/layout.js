dde948d387534a680c1e53434a947e00a0aaaedd
Rafshan Rakeeb <rakeebrafshan4@gmail.com>
2024-08-21 21:02:45 +0530

    chat

--- app/(root)/(routes)/layout.js
+++ app/(root)/(routes)/layout.js
@@ -8,7 +8,7 @@ import Chat from "@/components/chat";
 const MainPageLayout = ({children}) => {
     return ( 
         <>
-         <Chat/>
+     
         <ProductPreview/>
         {children}
         
