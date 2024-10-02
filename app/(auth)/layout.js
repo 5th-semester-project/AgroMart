39730e329ed767d00b6ce0080cbe26fd79e16e16
Neranjan Pushpakumara <neranjan.education@gmail.com>
2024-10-02 12:21:48 +0530

    changed images to lite weight format

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,8 +1,15 @@
 const AuthLayout = ({children}) => {
     return ( 
-        <div className="flex h-full items-center justify-center bg-green-300">
-            {children}
+        <div className="flex h-screen items-center justify-center relative">
+        <img
+          src="/bg-images/bg-login.webp"
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover  -z-10"
+        />
+        <div className="relative z-10 p-6 rounded-lg shadow-lg">
+          {children}
         </div>
+      </div>
      );
 }
  
