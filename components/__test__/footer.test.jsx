d274a941803401df30058279ac4d310b34e61c75
Neranjan Pushpakumara <neranjan.education@gmail.com>
2024-10-10 11:04:11 +0530

    starting to create CICD

---
+++ components/__test__/footer.test.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import Footer from "@/components/Footer";
+// import { FaFacebook, FaGithub, FaInstagram, FaTwitter, FaTwitch } from "react-icons/fa";
+
+
+// Footer.test.js
+describe("Footer Component", () => {
+    it("always passes", () => {
+      expect(true).toBe(true);
+    });
+  });
+  
